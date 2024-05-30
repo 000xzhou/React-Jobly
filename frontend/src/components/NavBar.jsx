b51db809771afddcaf32468bab58c15273840072
000xzhou <xzhou92@gmail.com>
2024-05-30 13:32:09 -0400

    css: added css to nav bar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,48 +1,58 @@
 import { Link } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import TokenContext from "../tokenContext";
 
 function NavBar() {
   const { currentUser } = useContext(TokenContext);
-  console.log(currentUser);
+  const [dropdown, setDropdown] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdown((prevState) => !prevState);
+  };
+  console.log(dropdown);
+
   return (
-    <>
+    <nav>
       <div>
         <Link to={`/`}>Jobly</Link>
       </div>
-      {/* if user not login  */}
-      <div>
-        <Link to={`/login`}>Login</Link>
-        <Link to={`/register`}>Register</Link>
-      </div>
-      {currentUser && (
+      {!currentUser ? (
         <div>
-          <div>{currentUser.username} v</div>
-          <div>
-            <div>{currentUser.username}</div>
+          <Link to={`/login`}>Login</Link>
+          <Link to={`/register`}>Register</Link>
+        </div>
+      ) : (
+        <div>
+          <div onClick={toggleDropdown} className="dropdownMenu">
+            {currentUser.username} v
+          </div>
+          <div
+            className="dropdownItem"
+            style={{ display: dropdown ? "flex" : "none" }}
+          >
             <Link to={`/users/${currentUser.username}`}>Edit profile</Link>
             <Link to={`/users/logout`}>Log out</Link>
 
             {currentUser.isAdmin ? (
-              <div>
-                <Link className="button" to={`/users`}>
+              <>
+                <Link className="navBtn" to={`/users`}>
                   List of users
                 </Link>
-                <Link className="button" to={`/jobs/new`}>
+                <Link className="navBtn" to={`/jobs/new`}>
                   Create A Job
                 </Link>
-                <Link className="button" to={`/companies/new`}>
+                <Link className="navBtn" to={`/companies/new`}>
                   Create A Company
                 </Link>
-                <Link className="button" to={`/users/new`}>
+                <Link className="navBtn" to={`/users/new`}>
                   Create A User
                 </Link>
-              </div>
+              </>
             ) : null}
           </div>
         </div>
       )}
-    </>
+    </nav>
   );
 }
 
