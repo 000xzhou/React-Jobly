e56f0bcb913453c9d768a0b29ccf439dc0b1f263
000xzhou <xzhou92@gmail.com>
2024-05-31 15:46:55 -0400

    added user to context to get job list

--- frontend/src/UserProvider.jsx
+++ frontend/src/UserProvider.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useContext } from "react";
 import useLocalStorage from "./hooks/useLocalStorage";
 import JoblyApi from "./api/api";
+import useAPI from "./hooks/useAPI";
 
 const UserContext = createContext();
 
@@ -12,8 +13,17 @@ export const UserProvider = ({ children }) => {
   // set token
   JoblyApi.token = token;
 
+  // get jobs from user - user.jobs
+  const [user, loading] = useAPI("getUser", currentUser.username);
+  // waiting for it to load before returning data
+  if (loading) {
+    return <div>Loading user data...</div>;
+  }
+
   return (
-    <UserContext.Provider value={{ currentUser, setCurrentUser, setToken }}>
+    <UserContext.Provider
+      value={{ currentUser, setCurrentUser, setToken, user }}
+    >
       {children}
     </UserContext.Provider>
   );
